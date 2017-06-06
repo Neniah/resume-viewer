c24c60031dbc10a823a46bc3bf878157405643b9
MARIA LOBILLO SANTOS <maria.lobillo.santos@gmail.com>
2017-06-06 13:12:39 +0200

    errors and parse json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
@@ -10,10 +11,38 @@ import Footer from './Components/Footer';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      foo: 'bar',
+      resumeData: {}
+    }
+  }
+
+  getResumeData(){
+    $.ajax({
+      url: 'http://localhost:3000/resumeData.json',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({resumeData: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(err);
+        alert(err);
+      }
+    });
+  }
+
+  componentDidMount(){
+    this.getResumeData();
+  }
+
   render() {
+    console.log(this.state.resumeData);
     return (
       <div className="App">
-        <Header />
+        <Header data={this.state.resumeData.main}/>
         <About />
         <Resume />
         <Portfolio />
